8a0c4b5c196130624ebc755dc3cbefe32fbd2d0d
Aqeel Bhat <aqeelbhat@icloud.com>
2021-02-08 18:15:52 +0400

    Add iOS app scaffold

---
+++ Scanner/Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scanner
+//
+//  Created by Aqeel B on 08/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
