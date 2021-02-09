600eb0be94196f9f0efad34aa75c5a7db1a7c115
Aqeel Bhat <aqeelbhat@icloud.com>
2021-02-09 15:50:21 +0400

    Add scan feature

--- Scanner/Scanner/ViewController.swift
+++ Scanner/Scanner/ViewController.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import WeScan
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func didPressInitButton(_sender: UIButton) {
+        let scannerViewController = ImageScannerController()
+        scannerViewController.imageScannerDelegate = self
+        present(scannerViewController, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
 }
 
+extension ViewController: ImageScannerControllerDelegate {
+    func imageScannerController(_ scanner: ImageScannerController, didFinishScanningWithResults results: ImageScannerResults) {
+        scanner.dismiss(animated: true)
+    }
+    
+    func imageScannerControllerDidCancel(_ scanner: ImageScannerController) {
+        scanner.dismiss(animated: true)
+    }
+    
+    func imageScannerController(_ scanner: ImageScannerController, didFailWithError error: Error) {
+        print(error)
+    }
+    
+}
